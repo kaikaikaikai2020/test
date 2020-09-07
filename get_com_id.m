9e275fd96bd5840ca24a1d2f8129816d450258f0
kaikaikaikai2020 <70093991+kaikaikaikai2020@users.noreply.github.com>
2020-09-07 20:10:34 +0800

    update

--- get_com_id.m
+++
@@ -1,9 +0,0 @@
-function [C,T_a,T_b] = get_com_id(A,B)
-    T_a = numel(A);
-    T_b = numel(B);
-    A = 1:T_a;
-    B = 1:T_b;
-    X = repmat(A,numel(B),1);
-    Y = repmat(B',numel(A),1);
-    C = [X(:),Y];
-end
