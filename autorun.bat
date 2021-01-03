8ba505f91ba498606d37755aa98e1442a8e24d57
kaikaikaikai2020 <70093991+kaikaikaikai2020@users.noreply.github.com>
2021-01-04 07:06:53 +0800

    xxx

--- autorun.bat
+++ autorun.bat
@@ -2,5 +2,8 @@ copy "Non Fungible ADR strategies.xlsx" C:\Users\ASUS\Documents\GitHub\test\ADR\
 cd C:\Users\ASUS\Documents\GitHub\test\ADR\ADR\ZX02-program\program\
 START /WAIT matlab -nosplash -nodesktop -r "try; A_ADR_flowchart; catch; end;quit();"
 cd C:\Users\ASUS\Documents\GitHub\test\
+cd C:\Users\ASUS\Documents\GitHub\test\ADR\ADR\ZX02-program\program\计算结果
+copy *.* G:\adr\ADR\ADR\ZX02-program\program\计算结果
+
 
 
